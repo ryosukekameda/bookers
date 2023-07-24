e90826d565ebc52ad7f1479a90592070fa662f41
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-07-24 10:35:57 +0000

    add bookers

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,25 @@
+class ListsController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/new'
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
