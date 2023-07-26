1b91298c0de25a4a17a944f5f3117a58c85e439b
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-07-26 17:43:55 +0000

    finish bookers

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/top' => 'homes#top'
+  get root :to => 'homes#top'
   resources :books
 end
