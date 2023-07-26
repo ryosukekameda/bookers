788e7b22dad43dfa988b41be86cb9773b5655ac4
ryosukekameda <138686682+ryosukekameda@users.noreply.github.com>
2023-07-26 07:44:30 +0000

    add bookers 07261645

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
   
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,6 +23,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
